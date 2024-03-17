2d1c24cfe21250040ab2199e455d69f8cab1c2c9
Halil Ücel <hucelsoft@gmail.com>
2024-03-17 16:08:33 +0300

    change todo

--- lib/index.dart
+++ lib/index.dart
@@ -5,7 +5,9 @@ export './theme/index.dart';
 export './view/index.dart';
 export './widget/index.dart';
 
+
 // TODO: WBoldText ve WNormalText için TextStyle varsa copyWith yoksa Manuel Style
 // TODO: Daire girilirken Apartman Kat ve Numarasından fazla girilemez.
 // TODO: Pro Plan ve diğerleri için RandomKey üret. Bu key ile UserID key birleştir.
 // TODO: Formları Form_Field ile oluştur.
+// TODO: IHive için valueListenable ekle.
