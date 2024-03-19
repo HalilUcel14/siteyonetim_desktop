4bd6ef34447cc632957205ef86ab11aca297401a
Halil Ücel <hucelsoft@gmail.com>
2024-03-19 17:22:47 +0300

    change todo

--- lib/index.dart
+++ lib/index.dart
@@ -8,5 +8,6 @@ export './widget/index.dart';
 // TODO: Daire girilirken Apartman Kat ve Numarasından fazla girilemez.
 // TODO: Pro Plan ve diğerleri için RandomKey üret. Bu key ile UserID key birleştir.
 // TODO: Formları Form_Field ile oluştur.
-// /// TODO: IHive için valueListenable ekle.
+// /// TODO: IHive için valueListenable ekle. -------------
 // TODO: Create EventCounter Read Write Delete ve Update işlemleri yap. , ChangeNotifier kullan.
+// TODO: Form alanları düzelt
